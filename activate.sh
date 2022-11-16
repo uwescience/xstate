48f7b731a91559116160b3165b01f3e1d21cc689
joseph-hellerstein <joseph.hellerstein@gmail.com>
2022-11-15 18:36:07 -0800

    Create environment

--- activate.sh
+++ activate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Sets the python path to run codes in this repo
 # Handle the sub-modules
+PYTHONPATH=""
 for d in common_python
   do
     cd $d
-    source setup_run.sh
+    PYTHONPATH=${PYTHONPATH}:`pwd`
     cd ..
   done
 #
@@ -13,3 +14,7 @@ cd python
 PYTHONPATH=${PYTHONPATH}:`pwd`
 export PYTHONPATH
 popd
+#
+conda config --set changeps1 True 
+source ~/opt/anaconda3/etc/profile.d/conda.sh
+conda activate xstate
